f90df8d8da927c0cca1a4340d4f9c519cfe20ae4
Nilber Remon <nilberremon@gmail.com>
2020-02-10 18:09:32 -0500

    Installed Node Libraries & Added Post Queries to Post Routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/all', (req, res) => {
-  res.send('Sending all posts!');
+const pgp = require('pg-promise')();
+const connection = {
+    host: 'localhost',
+    port: 5432,
+    database: 'facebook_db',
+}
+const db = pgp(connection);
+
+router.get('/all', async (req, res) => {
+  try {
+    let data = await db.any('SELECT * FROM posts')
+    res.status(200).json({
+      posts: data
+    }) 
+  } catch(error) {        
+    res.send('An error occurred: ' + error);
+    };
 });
 
 router.get('/:user_id', (req, res) => {
-  res.send('Sending all post from this user!');
+  try {
+    let data = await db.any(`SELECT * FROM posts WHERE poster_id = ${req.params.user_id}`)
+    res.status(200).json({
+      posts: data
+    }) 
+  } catch(error) {        
+    res.send('An error occurred: ' + error);
+    };
 });
 
-router.post('/register', (req, res) => {
-  res.send('Creating new post!');
+router.post('/register/:user_id', (req, res) => {
+  try {
+    let data = await db.none('INSERT INTO posts(poster_id, body) VALUES($1, $2)', [req.params.id, req.body[body]])
+    res.status(200).json({
+      message: "Post Created"
+    }) 
+  } catch(error) {        
+    res.send('An error occurred: ' + error);
+    };
 });
 
-
-/posts/all
-/posts/:user_id
-/posts/register
-
-
-module.exports = router;
