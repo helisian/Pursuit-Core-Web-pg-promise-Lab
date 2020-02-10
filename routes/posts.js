da71df910407e8a644276d140cc4be4c4f2c7d6f
Nilber Remon <nilberremon@gmail.com>
2020-02-10 12:00:49 -0500

    Added Project Starter Kit Files

---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/all', (req, res) => {
+  res.send('Sending all posts!');
+});
+
+router.get('/:user_id', (req, res) => {
+  res.send('Sending all post from this user!');
+});
+
+router.post('/register', (req, res) => {
+  res.send('Creating new post!');
+});
+
+
+/posts/all
+/posts/:user_id
+/posts/register
+
+
+module.exports = router;
